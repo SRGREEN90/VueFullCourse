eb0b3d1643938ca3ce5127dbf924b3a5d424c0a1
admin <1111>
2022-08-22 23:31:57 +0300

    "added fetchPosts method & axios"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
         <div class="app">
+
        <h1>Page with posts</h1>
+
+          <my-button @click="showDialog" style="margin: 15px 0">
+            Create a user
+          </my-button>
+
           <my-dialog v-model:show="dialogVisible">
             <post-form
                 @create="createPost"
@@ -10,7 +16,9 @@
          <post-list
              :posts="posts"
              @remove="removePost"
+             v-if="!isPostsLoading"
          />
+          <div v-else style="padding-left: 200px; color: red">LOADING...</div>
         </div>
 </template>
 
@@ -18,31 +26,52 @@
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
 import MyDialog from "@/components/UI/MyDialog";
+import MyButton from "@/components/UI/MyButton";
+import axios from "axios";
 
 //по дефолту всегда экспортируем объект
 export default {
   components: {
+    MyButton,
     MyDialog,
     PostForm, PostList
   },
     data() {
          return {
-      posts: [
-        {id: 1, title: 'JavaScript', body: 'JavaScript description'},
-        {id: 2, title: 'TypeScript', body: 'TypeScript description'},
-        {id: 3, title: 'ReactJS', body: 'ReactJS description'},
-        {id: 1, title: 'VueJS', body: 'VueJS description'}
-      ],
-           dialogVisible: false
+      posts: [],
+           dialogVisible: false,
+          isPostsLoading: false
     }
   },
   methods: {
     createPost(post) {
-        this.posts.push(post)
+        this.posts.push(post);
+        this.dialogVisible = false
     },
     removePost(post) {
       this.posts = this.posts.filter(p => p.id !== post.id)
     },
+    showDialog() {
+       this.dialogVisible = true
+    },
+   async fetchPosts() {
+        try {
+          this.isPostsLoading = true
+          setTimeout(async  () => {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+            this.posts = response.data
+            this.isPostsLoading = false
+          },1000)
+
+        } catch (e) {
+            alert('Error!!!')
+        } finally {
+
+        }
+    }
+  },
+   mounted(){
+     this.fetchPosts()
   }
 }
 </script>
