3bd288ef52b574c57c1a4b718c9c67e0691b9f84
admin <1111>
2022-08-24 22:04:17 +0300

    "added About, Main, UserPage"

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <div>Vue3</div>
+    <div class="navbar__btns">
+      <my-button>Users</my-button>
+      <my-button>About app</my-button>
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar{
+  height: 50px;
+  background-color: teal;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+}
+</style>
+
+
+<router-link to="/users">Users</router-link>
+<router-link to="/about">About app</router-link>
---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  <h1>This application was created as a result of training</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
